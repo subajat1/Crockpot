a74665fd83db1aaeb9b65777b1ac4a14e332747b
subajat1 <bayumunajat@outlook.com>
2019-11-16 12:51:02 +0800

    setup essential models and admins

--- src/cook/admin.py
+++ src/cook/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class BenefitAdmin(admin.ModelAdmin):
+    list_display = (
+        'health',
+        'hunger',
+        'sanity',
+        'nutrition',
+        'perish',
+    )
+admin.site.register(models.Benefit, BenefitAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'media',
+        'slug',
+    )
+admin.site.register(models.Category, CategoryAdmin)
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'media',
+        'slug',
+    )
+admin.site.register(models.Store, StoreAdmin)
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'media',
+        'slug',
+        'category',
+        'is_active'
+    )
+admin.site.register(models.Ingredient, IngredientAdmin)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'media',
+        'slug',
+        'benefit',
+        'cook_time',
+        'is_active'
+    )
+admin.site.register(models.Recipe, RecipeAdmin)
--- src/cook/models.py
+++ src/cook/models.py
@@ -1,3 +1,102 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Benefit(models.Model):
+    health = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    hunger = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    sanity = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    nutrition = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    perish = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+
+    class Meta:
+        ordering = ['health', 'hunger', 'sanity', 'nutrition', '-perish']
+        verbose_name = 'Benefit'
+        verbose_name_plural = 'Benefits'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=512, null=True, blank=True)
+    media = models.CharField(max_length=1024, null=True, blank=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.slug
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=512, null=True, blank=True)
+    media = models.CharField(max_length=1024, null=True, blank=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Store'
+        verbose_name_plural = 'Stores'
+
+    def __str__(self):
+        return self.slug
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Store, self).save(*args, **kwargs)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=512, null=True, blank=True)
+    media = models.CharField(max_length=1024, null=True, blank=True)
+    slug = models.SlugField(unique=True)
+    benefit_raw = models.ForeignKey(Benefit, related_name='ing_benefit_raw', on_delete=models.SET_NULL, null=True, blank=True)
+    benefit_cooked = models.ForeignKey(Benefit, related_name='ing_benefit_cooked', on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, related_name='ing_category', on_delete=models.SET_NULL, null=True, blank=True)
+    store = models.ManyToManyField('Store', related_name='ing_store')
+    created = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    log = models.TextField(blank=True, null=True, default='')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Ingredient'
+        verbose_name_plural = 'Ingredients'
+
+    def __str__(self):
+        return self.slug
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Ingredient, self).save(*args, **kwargs)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=512, null=True, blank=True)
+    media = models.CharField(max_length=1024, null=True, blank=True)
+    slug = models.SlugField(unique=True)
+    benefit = models.ForeignKey(Benefit, related_name='recipe_benefit', on_delete=models.SET_NULL, null=True, blank=True)
+    store = models.ManyToManyField('Store', related_name='recipe_store')
+    cook_time = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    ingredients = models.ManyToManyField('Ingredient', related_name='recipe_ingredient')
+    created = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    log = models.TextField(blank=True, null=True, default='')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
+
+    def __str__(self):
+        return self.slug
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Recipe, self).save(*args, **kwargs)
+
