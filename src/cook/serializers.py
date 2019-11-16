758c736ad523c6ab38ef53819c0e0dd746377ced
subajat1 <bayumunajat@outlook.com>
2019-11-16 21:07:20 +0800

    developed serializers

--- src/cook/serializers.py
+++ src/cook/serializers.py
@@ -7,4 +7,71 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ('url', 'id', 'username')
+        fields = ('url', 'id', 'username',)
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Category
+        fields = ('name', 'slug', 'icon', 'media',)
+
+
+class StoreSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Store
+        fields = ('name', 'slug', 'icon', 'media',)
+
+
+class IngredientSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Ingredient
+        fields = (
+            'name', 
+            'slug', 
+            'icon', 
+            'media_raw',
+            'health_raw',
+            'hunger_raw',
+            'sanity_raw',
+            'nutrition_raw',
+            'perish_raw',
+            'media_cooked',
+            'health_cooked',
+            'hunger_cooked',
+            'sanity_cooked',
+            'nutrition_cooked',
+            'perish_cooked',
+            'category',
+            'store',
+            'is_active',
+        )
+
+
+class RecipeSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.Recipe
+        fields = (
+            'name', 
+            'slug',
+            'media_raw',
+            'health_raw',
+            'hunger_raw',
+            'sanity_raw',
+            'nutrition_raw',
+            'perish_raw',
+            'store',
+            'cook_time',
+            'restrictions',
+            'is_active',
+        )
+
+
+class RecipeIngredientSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = models.RecipeIngredient
+        fields = ('name', 'recipe', 'ingredient',)
--- src/cook/urls.py
+++ src/cook/urls.py
@@ -7,12 +7,15 @@ from . import views
 
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
-
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'stores', views.StoreViewSet)
+router.register(r'ingredients', views.IngredientViewSet)
+router.register(r'recipes', views.RecipeViewSet)
+router.register(r'recipe_ingredients', views.RecipeIngredientViewSet)
 schema_view = get_schema_view(title='Don\'t Starve Crockpot API')
 
 urlpatterns = [
     url(r'^schema/$', schema_view),
     url(r'^api-auth/', include('rest_framework.urls')),
-
     url(r'^', include(router.urls)),
 ]
